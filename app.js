fafbcaf07d77c61ba4712be73336ee8fb9209ff0
Dominik Vagala <d.vagala@gmail.com>
2019-07-01 14:01:12 +0200

    services tabs messy

--- app.js
+++ app.js
@@ -1,2 +1,43 @@
 
-window.onblur = function() {window.onfocus= function () {location.reload(true)}};
+// window.onblur = function() {window.onfocus= function () {location.reload(true)}};
+
+services_section = document.getElementById("services-section-column2");
+
+summaryTabs = document.querySelectorAll(".summary-tab");
+detailsContentsP = document.querySelectorAll("#services-section-column2 > p");
+
+// console.log(summaryTabs);
+// console.log(detailsContentsP);
+
+summaryTabs.forEach((summaryTab, index) => {
+
+
+
+    console.dir(summaryTab);
+    // console.dir(blackTriangleIcon);
+
+    summaryTab.addEventListener("click", function(){
+        blackTriangleIcon = summaryTab.querySelector(".black-triangle");
+        greenTriangleIcon = summaryTab.querySelector(".green-triangle");
+        if (blackTriangleIcon.style.display == "none") {
+            blackTriangleIcon.style.display = "block";
+            greenTriangleIcon.style.display = "none";
+        }
+        else{
+            blackTriangleIcon.style.display = "none";
+            greenTriangleIcon.style.display = "block";
+        }
+    });
+});
+
+
+
+// summary_1 = document.getElementById("summary-1");
+// details_content_1 = document.getElementById("details-content-1");
+
+// summary_1.addEventListener("click", function(){
+//     console.log("hooo");
+//     details_content_1.innerHTML = "heee";
+//     details_content_1.style.display = "none";
+// });
+

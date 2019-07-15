45c02ed37397b9baeff6ce40731ac4dfc40528bf
Dominik Vagala <d.vagala@gmail.com>
2019-07-15 22:06:18 +0200

    tidy up files working

--- app.js
+++ app.js
@@ -25,11 +25,10 @@ function detailsTabTransitionWorkarond() {
 
     var summaryTabs = document.querySelectorAll(".summary-tab");
     var detailTabs = document.querySelectorAll(".details-tab");
-    console.log(document.querySelector("body"));
 
     for (let index = 0; index < summaryTabs.length; index++) {
 
-    var summaryTab = summaryTabs[index];
+        var summaryTab = summaryTabs[index];
         var detailTab = detailTabs[index];
 
         detailTab.style.height = "100%";
@@ -42,17 +41,7 @@ function detailsTabTransitionWorkarond() {
     }
 }
 
-
 detailsTabTransitionWorkarond();
-
-
-// For smooth scroll to element
-// $('a').click(function(){
-//     $('html, body').animate({
-//         scrollTop: $( $(this).attr('href') ).offset().top
-//     }, 500);
-//     return false;
-// });
 
 
 // When the user scrolls down 20px from the top of the document, show the button
@@ -69,4 +58,3 @@ if (!("ontouchstart" in document.documentElement)) {
     document.documentElement.className += "no-touch";
 }
 
-window.scrollBy({ top: 1000, left: 0, behavior: 'smooth' });
